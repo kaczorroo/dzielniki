c428f66ea3f3825e15f5c5f8f5f6549982a193df
DanielKaczorowski <email@daniel.com.pl>
2015-12-05 15:04:15 +0100

    gotowe

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Start programu.");
+        System.out.println("Program do wypisywania dzielników liczby całkowitej.");
+        System.out.println("Podaj liczbę całkowitą.");
+        Scanner liczba = new Scanner(System.in);
+        int n = liczba.nextInt();
+        liczba.close();
+        for (int i = 1; i<=n; i++){
+            if (n%i==0) {
+                System.out.println(i);
+            }
+        }
     }
 }
